3125482902b774151479fa76053e15ed610c9ba3
mcrank1 <mcrank@luc.edu>
2024-08-08 14:10:08 -0500

    added the rest of the commit

--- app/src/main/java/edu/luc/etl/cs313/android/simplestopwatch/model/clock/DefaultClockModel.java
+++ app/src/main/java/edu/luc/etl/cs313/android/simplestopwatch/model/clock/DefaultClockModel.java
@@ -42,17 +42,32 @@ public class DefaultClockModel implements ClockModel {
         // The clock model runs onTick every 1000 milliseconds
         timer.schedule(new TimerTask() {
             @Override public void run() {
-                // fire event
-                listener.onTick();
-                elapsedTime += System.currentTimeMillis() - startTime;
+                long currentTime = System.currentTimeMillis();
+                elapsedTime += currentTime - startTime; //get elapsed time
+                //update start time for next tick
+                startTime = currentTime;
+
+
+                //fire the tick event only if one second has elapsed
+                if (elapsedTime >= speed) {
+                    //subtract one second
+                    elapsedTime -= speed;
+                    //notify listener
+                    listener.onTick();
+                }
             }
-        }, /*initial delay*/ speed - (elapsedTime % speed), /*periodic delay*/ speed);
+                // fire event
+                /*listener.onTick();
+                elapsedTime += System.currentTimeMillis() - startTime;
+            }*/
+        }, /*initial delay*/ speed, /*- (elapsedTime % speed),*/ /*periodic delay*/ speed);
     }
 
     @Override
     public void stop() {
         if (timer != null) {
             timer.cancel();
+            timer = null;
 
             //calculate the total elapsed time by subtracting startTime from elapsedtime
             elapsedTime = System.currentTimeMillis() - startTime;
