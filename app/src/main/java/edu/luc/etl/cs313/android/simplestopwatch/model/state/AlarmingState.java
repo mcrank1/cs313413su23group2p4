a1a5a92f4af99618db508f46b1337ceacd41719e
mcrank1 <mcrank@luc.edu>
2024-08-08 16:40:55 -0500

    added javadoc for AlarmingState

--- app/src/main/java/edu/luc/etl/cs313/android/simplestopwatch/model/state/AlarmingState.java
+++ app/src/main/java/edu/luc/etl/cs313/android/simplestopwatch/model/state/AlarmingState.java
@@ -1,4 +1,13 @@
 package edu.luc.etl.cs313.android.simplestopwatch.model.state;
+/**
+ * Represents the alarming state in the stopwatch state machine.
+ * In this state, the stopwatch is continuously beeping to indicate the occurrence of an alarm.
+ * The state transitions to the stopped state when the button is pressed.
+ * Behavior specific to the alarming state includes
+ * handling button presses and timer ticks, as well as updating the UI to reflect
+ * the current runtime. The alarming state does not transition to another state
+ * except when the button is pressed, at which it moves to the stopped state.
+ */
 
 import edu.luc.etl.cs313.android.simplestopwatch.R;
 
